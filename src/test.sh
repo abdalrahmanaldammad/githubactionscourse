eff751fd96cd673a5f2a4b847f662d89d0d22eb4
abdulrahman-al-dammad@abdalrahman <abdalrahmanaldammad@gmail.com>
2025-07-22 18:26:32 +0300

    create a new node app

---
+++ src/test.sh
@@ -0,0 +1,14 @@
+EXPECTED="Hello abood"
+
+OUTPUT=$(node -e "console.log(require('./src/app')('abood')")
+
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+
+  echo  "test passed"
+
+  exit 0
+else
+  echo "test faild"
+  exit 1
+
+fi
